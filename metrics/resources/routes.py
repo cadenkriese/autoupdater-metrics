371e134b8877a7d9a21d7cca4bfc016a5a7b72d2
flogic <gamerking195@gmail.com>
2020-01-06 23:59:36 -0700

    Refactor API routes.

--- metrics/resources/routes.py
+++ metrics/resources/routes.py
@@ -6,8 +6,8 @@ from .plugins import PluginsAPI, UpdatesAPI, PluginAPI
 
 def initialize_routes(api):
     """Initializes flask-restful routes for the current app."""
-    api.add_resource(PluginsAPI, "/plugins")
-    api.add_resource(PluginAPI, "/plugins/<plugin_name>")
-    api.add_resource(UpdatesAPI, "/plugins/<plugin_name>/updates")
+    api.add_resource(PluginsAPI, "/updater-metrics/v1/plugins")
+    api.add_resource(PluginAPI, "/updater-metrics/v1/plugins/<plugin_name>")
+    api.add_resource(UpdatesAPI, "/updater-metrics/v1/plugins/<plugin_name>/updates")
 
-    api.add_resource(AuthAPI, "/plugins/auth")
+    api.add_resource(AuthAPI, "/updater-metrics/v1/auth")
